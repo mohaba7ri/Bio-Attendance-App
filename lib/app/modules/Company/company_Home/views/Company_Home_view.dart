231a0cf4c0a4b2b293f5aa4e712222348f61ca61
EnAmeenAlsharafi <en.ameenalsharafi@gmail.com>
2023-01-21 21:10:15 +0300

    commit

--- lib/app/modules/Company/company_Home/views/Company_Home_view.dart
+++ lib/app/modules/Company/company_Home/views/Company_Home_view.dart
@@ -8,96 +8,105 @@ import '../../../../routes/app_pages.dart';
 import '../../../../style/app_color.dart';
 import '../../../../util/images.dart';
 import '../../../../widgets/Menu_tile.dart';
+import '../../company_settings/controller/company_seting_controlleer.dart';
+import '../../company_settings/view/company_setting_view.dart';
 import '../controllers/Company_Home_controller.dart';
 
 class CompanyHomeView extends GetView<CompanyHomeController> {
   final pageIndexController = Get.find<PageIndexController>();
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> companyStream =
+        FirebaseFirestore.instance.collection('company').snapshots();
     return Scaffold(
       backgroundColor: AppColor.primary,
       body: CustomeAppbar(
         backButton: true,
         backRout: () => Get.toNamed(Routes.HOME),
-        mainWidget: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-          stream: controller.streamUser(),
-          builder: (context, snapshot) {
+        mainWidget: StreamBuilder<QuerySnapshot>(
+          stream: companyStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
                 return Center(child: CircularProgressIndicator());
               case ConnectionState.active:
               case ConnectionState.done:
-                Map<String, dynamic> userData = snapshot.data!.data()!;
                 return Container(
                   color: AppColor.whiteColor,
                   child: ListView(
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(),
-                    padding: EdgeInsets.symmetric(vertical: 36),
-                    children: [
-                      SizedBox(height: 16),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 16, bottom: 30),
-                            child: Text(
-                              userData["name"],
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> companyData =
+                          document.data()! as Map<String, dynamic>;
+                      return Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Welcome To :\n' +
+                                  '${companyData['name']}' +
+                                  ' company',
                               style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w500),
+                                  color: AppColor.secondarySoft,
+                                  fontFamily: 'cairo',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800),
                             ),
-                          ),
-                          Text(
-                            userData["job"],
-                            style: TextStyle(color: AppColor.secondarySoft),
-                          ),
-                        ],
-                      ),
-                      MenuTile(
-                        isDanger: true,
-                        title: 'View Details',
-                        icon: Image.asset(
-                          Images.changePassword,
-                          color: AppColor.primarySoft,
-                          width: 20,
-                          height: 20,
+                            SizedBox(
+                              height: 20,
+                            ),
+                            MenuTile(
+                              isDanger: true,
+                              title: 'View Details',
+                              icon: Image.asset(
+                                Images.changePassword,
+                                color: AppColor.primarySoft,
+                                width: 20,
+                                height: 20,
+                              ),
+                              onTap: () => Get.toNamed(Routes.COMPANY_DETAILS),
+                            ),
+                            MenuTile(
+                              isDanger: true,
+                              title: 'Edit Information',
+                              icon: Image.asset(
+                                Images.editProfile,
+                                color: AppColor.primarySoft,
+                                width: 20,
+                                height: 20,
+                              ),
+                              onTap: () {
+                                Get.toNamed(Routes.ADD_COMPANY_SETTING);
+                              },
+                            ),
+                            MenuTile(
+                              isDanger: true,
+                              title: 'Settings',
+                              icon: Image.asset(
+                                Images.editProfile,
+                                color: AppColor.primarySoft,
+                                width: 20,
+                                height: 20,
+                              ),
+                              onTap: () {
+                                Get.put(CompanySettingController());
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => CompanySettingView());
+                              },
+                            ),
+                            Container(
+                              height: 1,
+                              color: AppColor.primaryExtraSoft,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            )
+                          ],
                         ),
-                        onTap: () => Get.toNamed(Routes.COMPANY_DETAILS),
-                      ),
-                      MenuTile(
-                        isDanger: true,
-                        title: 'Edit Information',
-                        icon: Image.asset(
-                          Images.editProfile,
-                          color: AppColor.primarySoft,
-                          width: 20,
-                          height: 20,
-                        ),
-                        onTap: () {
-                          Get.toNamed(Routes.ADD_COMPANY_SETTING);
-                        },
-                      ),
-                      MenuTile(
-                        isDanger: true,
-                        title: 'Settings',
-                        icon: Image.asset(
-                          Images.editProfile,
-                          color: AppColor.primarySoft,
-                          width: 20,
-                          height: 20,
-                        ),
-                        onTap: () {
-                          Get.toNamed(Routes.ADD_COMPANY_SETTING);
-                        },
-                      ),
-                      Container(
-                        height: 1,
-                        color: AppColor.primaryExtraSoft,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      )
-                    ],
+                      );
+                    }).toList(),
                   ),
                 );
 
