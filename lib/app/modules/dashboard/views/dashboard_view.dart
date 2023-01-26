204549a358f9560fd9883c9edb248f159c3aed76
EnAmeenAlsharafi <en.ameenalsharafi@gmail.com>
2023-01-25 18:42:36 -0800

    commit

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -37,192 +37,264 @@ class DashboardView extends GetView<DashboardController> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
+      body: Container(
+        color: Colors.grey[100],
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: AppColor.whiteColor,
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
                             blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                            offset: Offset(5, 6),
+                          )
+                        ],
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Employees \n 12',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
                     ),
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                    Spacer(),
+                    Container(
+                      height: 100,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: AppColor.whiteColor,
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 2.0,
+                            offset: Offset(5, 6),
+                          )
+                        ],
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Employees \n 12',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
                     ),
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2.0,
+                              offset: Offset(5, 6),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Employees \n 12',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Container(
+                          height: 100,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: AppColor.whiteColor,
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 2.0,
+                                offset: Offset(5, 6),
+                              )
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              'Employees \n 12',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w800,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
+                    ],
                   ),
-                  Spacer(),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2.0,
+                              offset: Offset(5, 6),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Employees \n 12',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                      gradient: LinearGradient(
-                          colors: [Colors.indigo, Colors.blueAccent]),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 2.0,
-                            offset: Offset(2.0, 2.0))
-                      ],
-                    ),
+                      Spacer(),
+                      Container(
+                        height: 100,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2.0,
+                              offset: Offset(5, 6),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Employees \n 12',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2.0,
+                              offset: Offset(5, 6),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Employees \n 12',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Container(
+                        height: 100,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2.0,
+                              offset: Offset(5, 6),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            'Employees \n 12',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
