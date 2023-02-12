e53d88a8ca90cde86fc4bd5b91bc50c5fcdf7fdd
EnAmeenAlsharafi <en.ameenalsharafi@gmail.com>
2023-02-12 14:56:24 +0300

    c

--- lib/main.dart
+++ lib/main.dart
@@ -65,9 +65,7 @@ void main() async {
             return GetMaterialApp(
               //  title: "Application",
               debugShowCheckedModeBanner: false,
-              initialRoute: snapshot.data != null
-                  ? Routes.LIST_VIEW_REQUESTS
-                  : Routes.LIST_VIEW_REQUESTS,
+              initialRoute: snapshot.data != null ? Routes.LOGIN : Routes.LOGIN,
               getPages: AppPages.routes,
               locale: languageController.locale,
               translations: Messages(languages: _languages),
