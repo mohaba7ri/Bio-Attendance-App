257e4cd9964f68d6d7104197d1d1bc061aa36cf6
EnAmeenAlsharafi <en.ameenalsharafi@gmail.com>
2023-02-13 23:01:52 +0300

    commit

--- lib/app/modules/Employees/employee_Home/views/Employee_Home_view.dart
+++ lib/app/modules/Employees/employee_Home/views/Employee_Home_view.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:presence/app/controllers/page_index_controller.dart';
-import 'package:presence/app/widgets/custom_appbar.dart';
+import 'package:presence/app/widgets/custom_widget.dart';
 
 import '../../../../routes/app_pages.dart';
 import '../../../../style/app_color.dart';
@@ -16,7 +16,7 @@ class EmployeeHomeView extends GetView<EmployeeHomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.primary,
-      body: CustomeAppbar(
+      body: CustomeWidget(
         title: 'Employee_Management'.tr,
         backButton: true,
         backRout: () => Get.toNamed(Routes.HOME),
--- lib/app/widgets/custom_appbar.dart
+++ lib/app/widgets/custom_appbar.dart
@@ -1,101 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:presence/app/modules/languages/controller/languages_controller.dart';
 
 import '../style/app_color.dart';
-import '../util/dinmensions.dart';
-import '../util/images.dart';
-import '../util/styles.dart';
 
-class CustomeAppbar extends StatelessWidget {
-  final Widget mainWidget;
-  bool backButton;
-  String? title;
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  final title;
+  final bool isBackBotton;
+  final bool isaction;
+  final action;
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.action,
+    this.isBackBotton = false,
+    this.isaction = false,
+  });
 
-  Function()? backRout;
-  CustomeAppbar(
-      {required this.mainWidget,
-      this.backButton = false,
-      this.backRout,
-      this.title});
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<LanguagesController>(
-      builder: (controller) => Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Center(
-                child: Container(
-                  width: Dimensions.WEB_MAX_WIDTH,
-                  height: MediaQuery.of(context).size.height * 25 / 100,
-                  color: AppColor.primary,
-                ),
-              ),
-              title != null
-                  ? Positioned(
-                      top: MediaQuery.of(context).padding.top + 10,
-                      left: 0,
-                      right: 0,
-                      child: Text(
-                        title!,
-                        textAlign: TextAlign.center,
-                        style: robotoRegular.copyWith(
-                            fontSize: Dimensions.fontSizeLarge,
-                            color: Theme.of(context).cardColor),
-                      ),
-                    )
-                  : SizedBox(),
-              SizedBox(
-                width: context.width,
-                height: MediaQuery.of(context).size.height * 25 / 100,
-                child: Center(
-                    child: Image.asset(Images.coverAppbar,
-                        height: MediaQuery.of(context).size.height * 25 / 100,
-                        width: Dimensions.WEB_MAX_WIDTH,
-                        fit: BoxFit.fill)),
-              ),
-              backButton == true && controller.isLtr == false
-                  ? Positioned(
-                      top: MediaQuery.of(context).padding.top,
-                      right: 10,
-                      child: IconButton(
-                        icon: Icon(Icons.arrow_back_ios,
-                            color: AppColor.whiteColor, size: 20),
-                        onPressed:
-                            backRout != null ? backRout : () => Get.back(),
-                      ),
-                    )
-                  : backButton == true && controller.isLtr == true
-                      ? Positioned(
-                          top: MediaQuery.of(context).padding.top,
-                          left: 10,
-                          child: IconButton(
-                            icon: Icon(Icons.arrow_back_ios,
-                                color: AppColor.whiteColor, size: 20),
-                            onPressed:
-                                backRout != null ? backRout : () => Get.back(),
-                          ),
-                        )
-                      : SizedBox(),
-            ],
-          ),
-          Container(
-            height: 20,
-            decoration: BoxDecoration(
-                color: AppColor.whiteColor,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20))),
-          ),
-          Expanded(
-              child: Container(
-            color: AppColor.whiteColor,
-            child: mainWidget,
-          ))
-        ],
+    return AppBar(
+      title: Text(
+        'Employees on Vacation',
+        style: TextStyle(
+          color: AppColor.secondary,
+          fontSize: 14,
+        ),
+      ),
+      leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: AppColor.blackColor,
+          )),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      actions: action,
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(1),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 1,
+          color: AppColor.secondaryExtraSoft,
+        ),
       ),
     );
   }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
